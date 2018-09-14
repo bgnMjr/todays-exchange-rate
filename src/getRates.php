b9dc3a38c7aee6f0886476a6602a90e07060f7fa
BM <bm@mj.com>
2018-09-14 16:15:08 +0200

    added comments and some fixes

--- src/getRates.php
+++ src/getRates.php
@@ -2,14 +2,17 @@
 /*
  * Example taken from https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html#dev
  * and slightly modified
+ * Required: config option allow_url_fopen=On (default)
+ * To get current data, change simplexml_load_file argument to the following:
+ * http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml
+ * (the file is updated at around 16:00 CET)
  */
-  //Required: config option allow_url_fopen=On (default)
-  //the file is updated at around 16:00 CET
-  $XML=simplexml_load_file("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+  $XML=simplexml_load_file("../eurofxref-daily.xml");
 
   //The following array will be encoded as JSON and passed through responseText
   $currenciesRates = array("date"=>(string)$XML->Cube->Cube[0]["time"]);
-  
+
+  //read values into $currenciesRates array
   foreach($XML->Cube->Cube->Cube as $rate){
     $currenciesRates[$rate["currency"]->__toString()] =
                                                   $rate["rate"]->__toString();
